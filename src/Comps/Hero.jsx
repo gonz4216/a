d3bc165ba67f9884dffd056e42d67b5e3fc1f7a4
gonz4216 <ethgon19@gmail.com>
2022-11-19 17:44:36 -0500

    main

---
+++ src/Comps/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './hero.css'
+const Hero = () => {
+  return (
+    <>
+     <div className='hero'>
+
+
+        </div>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/Comps/Info.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './info.css'
+const Info = () => {
+  return (
+    <div className='info'>Hello, Test</div>
+  )
+}
+
+export default Info
