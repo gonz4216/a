616008c80c0148da7a49ed5366512cc6c68ee4fc
gonz4216 <ethgon19@gmail.com>
2022-11-20 17:38:56 -0500

    new pages

---
+++ src/Comps/annInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ann.css'
+const annInfo = () => {
+  return (
+    <div className='annInfo'>
+
+         
+    </div>
+  )
+}
+
+export default annInfo
---
+++ src/Comps/solarInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const solarInfo = () => {
+  return (
+    <div>solarInfo</div>
+  )
+}
+
+export default solarInfo
