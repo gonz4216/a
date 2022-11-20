797e0e02e5b8585c403071a1935daf8b9a9d15d8
KMalizei <kkouroshm@yahoo.com>
2022-11-20 16:00:58 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
- 
-import Hero from './Comps/Hero';
-import Info from './Comps/Info';
-import './index.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from "./Comps/Hero";
+import SolarInfo from "./Comps/solarInfo";
+import AnnInfo from "./Comps/annInfo"
+import "./index.css";
 
- 
 function App() {
-  function solution(str){
-  
-  }
   return (
-    <div className="App">
-       
-    <Hero />
-    <Info />
-  
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/solarinfo" element={<SolarInfo />} />
+        <Route path="/anninfo" element={<AnnInfo />} />
+      </Routes>
+    </Router>
   );
+}
 
- }
-
- 
 export default App;
